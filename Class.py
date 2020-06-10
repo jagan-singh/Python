6f84f7cdfd9970f3ff54a50dac3362516d2b6314
Jagandeep Singh <jagansingh93@gmail.com>
2020-06-10 16:55:01 -0400

    Tutorial 2 completed

--- Class.py
+++ Class.py
@@ -1,22 +1,29 @@
 #Object Oriented Programming
 class Employee:
 
+    raise_amount = 1.04
+    num_of_emps = 0
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = first + '.'+ last + '@company.com'
+        Employee.num_of_emps +=1
 
     def fullName(self):
         return '{} {}'.format(self.first, self.last)
 
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+print(Employee.num_of_emps)
 
 emp1 = Employee('John', 'Doe', 50000 )
 emp2 = Employee('j','d',9000)
+emp = Employee('Test','T',8000)
 
-print(emp1.first)
-print(emp1.last)
 print(emp1.pay)
-print(emp1.email)
-print(emp1.fullName())
-print(emp2.fullName())
+emp1.apply_raise()
+print(emp1.pay)
+print(Employee.num_of_emps)
