524a77b5e058b9fb0ab6cbcd91dd3d0eea606aa3
Jagandeep Singh <jagansingh93@gmail.com>
2020-06-13 20:42:17 -0400

    tutorial 3 completed

--- Class.py
+++ Class.py
@@ -14,16 +14,64 @@ class Employee:
     def fullName(self):
         return '{} {}'.format(self.first, self.last)
 
+
+    #instance method
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
+    #class method
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls,emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first,last,pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+
+
+
+
 print(Employee.num_of_emps)
+
+Employee.set_raise_amt(1.05)
 
 emp1 = Employee('John', 'Doe', 50000 )
 emp2 = Employee('j','d',9000)
-emp = Employee('Test','T',8000)
+emp3 = Employee('Test','T',8000)
+
+emp_str1 = 'Steven-Smith-80000'
+emp_str2 = 'Jane-Doe-90000'
+
+emp4 = Employee.from_string(emp_str1)
+
+
 
 print(emp1.pay)
 emp1.apply_raise()
 print(emp1.pay)
 print(Employee.num_of_emps)
+
+print(emp1.raise_amount)
+print(emp2.raise_amount)
+print(emp3.raise_amount)
+print(emp4.email)
+
+import datetime
+my_date = datetime.date(2020,06,15)
+
+print(Employee.is_workday(my_date))
+
+
+
+
+
+
+
